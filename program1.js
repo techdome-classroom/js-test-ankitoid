d2c726f33f501a331218c36a6ca782214bd7a5a4
ankitoid <ankitsinghghoshi1234@gmail.com>
2024-01-05 00:01:36 -0800

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+        const LSymbol = [];
+
+        for (let i = 0; i < s.length; i++) {
+
+            if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+                // storing brackets to the stack 
+                LSymbol.push(s[i]);
+            }
+
+            else if (s[i] === ')' && LSymbol.length !== 0 && LSymbol[LSymbol.length - 1] === '(') {
+                LSymbol.pop();
+            } else if (s[i] === '}' && LSymbol.length !== 0 && LSymbol[LSymbol.length - 1] === '{') {
+                LSymbol.pop();
+            } else if (s[i] === ']' && LSymbol.length !== 0 && LSymbol[LSymbol.length - 1] === '[') {
+                LSymbol.pop();
+            }
+
+            else {
+                return false;
+            }
+        }
+        return LSymbol.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,8 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
-};
+    // create a map 
+        const RMap = new Map();
+        
+        RMap.set('I', 1);
+        RMap.set('V', 5);
+        RMap.set('X', 10);
+        RMap.set('L', 50);
+        RMap.set('C', 100);
+        RMap.set('D', 500);
+        RMap.set('M', 1000);
 
+        const n = s.length;
+
+        let num = RMap.get(s[n - 1]);
+
+        for (let i = n - 2; i >= 0; i--) {
+            //if the condition is true it add the value 
+            if (RMap.get(s[i]) >= RMap.get(s[i + 1])) {
+                num += RMap.get(s[i]);
+
+            }
+            // else decrese the value
+            else {
+                
+                num -= RMap.get(s[i]);
+            }
+        }
+        return num;
+    };
 
 module.exports={romanToInt}
